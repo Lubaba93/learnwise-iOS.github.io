8dc573817041299833d16d1ae08bc04998840899
Lubaba93 <lubaba@nineleaps.com>
2017-06-06 20:55:13 +0530

    Add configuration for dev, qa and preoduction environments

--- Learnwise/ViewController.swift
+++ Learnwise/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let dictionary = Bundle.main.infoDictionary {
+            let appName = dictionary["APP_NAME"] as? String
+            print("App name: \(String(describing: appName)))")
+
+            let backend = (dictionary["BACKEND_URL"] as? String)?.replacingOccurrences(of: "\\", with: "")
+            print("backend: \(String(describing: backend))")
+        }
     }
 
     override func didReceiveMemoryWarning() {
