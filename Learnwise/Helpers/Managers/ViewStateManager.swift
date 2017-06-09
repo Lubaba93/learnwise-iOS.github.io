0b86b1183788a85315079951ebce6f83d263b84d
Lubaba93 <lubaba@nineleaps.com>
2017-06-10 00:18:49 +0530

    Add doc blocks for State Manager

--- Learnwise/Helpers/Managers/ViewStateManager.swift
+++ Learnwise/Helpers/Managers/ViewStateManager.swift
@@ -11,43 +11,41 @@ import UIKit
 class ViewStateManager {
     var viewStore: [String: UIView] = [:]
 
-    /**
-     Returns the view for a given state
-     
-     - returns: UIView for a given state
-     */
+    /// Returns the view for a given state
+    ///
+    /// - Parameter state: state of the view
+    /// - Returns: UIView for a given state
     public func viewForState(_ state: String) -> UIView? {
         return viewStore[state]
     }
 
-    /**
-     Associates a view for the given state
-     */
+    /// Associates a view for the given state
+    ///
+    /// - Parameters:
+    ///   - view: view to be added
+    ///   - state: state of the view
+    ///   - superview: super view
     public func addView(_ view: UIView, forState state: String, superview: UIView) {
         viewStore[state] = view
         superview.addSubview(view)
     }
 
-    /**
-     Removes the view for the given state
-     */
+    /// Removes the view for the given state
+    ///
+    /// - Parameter state: state of the view
     public func removeViewForState(_ state: String) {
         viewStore[state]?.removeFromSuperview()
         viewStore[state] = nil
     }
 
-    /**
-     Hide all views
-     */
+    /// Hide all views
     public func hideAllViews() {
         for (_, view) in self.viewStore {
             view.isHidden = true
         }
     }
-
-    /**
-     Remove all views
-     */
+    
+    /// Remove all views
     public func removeAllViews() {
         for (_, view) in self.viewStore {
             view.removeFromSuperview()
